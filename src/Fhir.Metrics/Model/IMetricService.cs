be814ed1504ab26e30eed165e79fe3f862b9e03e
Kasdejong <k.j2.de.jong@student.vu.nl>
2024-05-28 10:43:54 +0200

    Added nullability attributes to IMetricService.cs

--- src/Fhir.Metrics/Model/IMetricService.cs
+++ src/Fhir.Metrics/Model/IMetricService.cs
@@ -1,21 +1,23 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Fhir.Metrics;
 
 public interface IMetricService
 {
-    public bool TryCanonicalize((string value, string unit, string codesystem) quantity, out (string value, string unit, string codesystem)? canonical);
+    public bool TryCanonicalize((string value, string unit, string codesystem) quantity, [NotNullWhen(true)] out (string value, string unit, string codesystem)? canonical);
     
-    public bool TryDivide((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, out (string value, string unit, string codesystem)? result);
+    public bool TryDivide((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, [NotNullWhen(true)] out (string value, string unit, string codesystem)? result);
     
-    public bool TryMultiply((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, out (string value, string unit, string codesystem)? result);
+    public bool TryMultiply((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, [NotNullWhen(true)] out (string value, string unit, string codesystem)? result);
     
-    public bool TryCompare((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, out int? result);
+    public bool TryCompare((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, [NotNullWhen(true)] out int? result);
     
     
     // from here on - not yet implemented in the old one
     
-    public bool TryConvertTo((string value, string unit, string codesystem) quantity, string targetUnit, out (string value, string unit, string codesystem)? converted);
+    public bool TryConvertTo((string value, string unit, string codesystem) quantity, string targetUnit, [NotNullWhen(true)] out (string value, string unit, string codesystem)? converted);
     
-    public bool TrySubtract((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, out (string value, string unit, string codesystem)? result);
+    public bool TrySubtract((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, [NotNullWhen(true)] out (string value, string unit, string codesystem)? result);
     
-    public bool TryAdd((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, out (string value, string unit, string codesystem)? result);
+    public bool TryAdd((string value, string unit, string codesystem) quantity1, (string value, string unit, string codesystem) quantity2, [NotNullWhen(true)] out (string value, string unit, string codesystem)? result);
 }
